9ea3d4a21458598fadfa3d7cf3d2705b8f308f4f
Serge Babayan <babayan.serge@gmail.com>
2025-03-15 02:17:58 +0300

    Add class Article

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text(300), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 @app.route('/')
 @app.route('/home')
